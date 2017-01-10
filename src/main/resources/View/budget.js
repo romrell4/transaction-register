7dc4b910750c3fc58e504b7ad187bfeca9479c0e
Eric Romrell <eric_romrell@byu.edu>
2017-01-09 17:05:47 -0700

    Updated view and added transactions logic

--- src/main/resources/View/budget.js
+++ src/main/resources/View/budget.js
@@ -2,7 +2,7 @@
  * Created by eric on 12/4/16.
  */
 angular.module("root", [])
-	.controller("index", function($http) {
+	.controller("app", function($http) {
 		var vm = this;
 
 		$http.get("https://transaction-register.herokuapp.com/categories/active")
--- src/main/resources/View/transactions.js
+++ src/main/resources/View/transactions.js
@@ -2,7 +2,13 @@
  * Created by eric on 12/4/16.
  */
 angular.module("root", [])
-	.controller("index", function($http) {
+	.controller("app", function($http) {
 		var vm = this;
 
+		$http.get("https://transaction-register.herokuapp.com/transactions")
+			.then(function(response) {
+				vm.transactions = response.data;
+			}, function(error) {
+				console.log("Error")
+			});
 	});
